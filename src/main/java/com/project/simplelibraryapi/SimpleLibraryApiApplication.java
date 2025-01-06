cf5c82dff5e7910a53b77c688a8cf500e1a5633c
yoonho0922 <yoonho5684@gmail.com>
2025-01-06 14:28:24 +0900

    init

---
+++ src/main/java/com/project/simplelibraryapi/SimpleLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.simplelibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLibraryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/simplelibraryapi/SimpleLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.simplelibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
